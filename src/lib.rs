849f19a7cee04aae6d8029ebdaf64682cbc20157
RAprogramm <andrey.rozanov.vl@gmail.com>
2025-12-22 15:44:07 +0700

    feat: initial bible module

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod loader;
+pub mod reading_plan;
+pub mod repository;
+pub mod search;
+
+pub use loader::BibleLoader;
+pub use reading_plan::ReadingPlan;
+pub use repository::BibleRepository;
+pub use search::BibleSearch;
